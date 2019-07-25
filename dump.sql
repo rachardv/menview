0a8a8625a61a5a6c87c67d51158daf86442d5e79
James Hopkins <jehopkin@sfu.ca>
2019-07-24 20:44:35 -0700

    updated dump.sql to take csv files

--- dump.sql
+++ dump.sql
@@ -1,19 +1,5 @@
+COPY restaurants(name, description, address, lat, long)
+FROM './data/restaurants.csv' DELIMITER ',' CSV HEADER;
 
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: admin
---
-
-COPY public.users (id, name, email, password, account_type) FROM stdin;
-1	bob	abc@123.ca	password account_type
-2	terry	cde@123.ca	notpassword	beef
-3	janice	fgh@123.ca	safepassword	cheese
-\.
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
+COPY dishes(dish_id, dish_name, dish_description, dish_rating, restaurant_name)
+FROM './data/dishess.csv' DELIMITER ',' CSV HEADER;
